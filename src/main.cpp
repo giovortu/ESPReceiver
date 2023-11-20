691632ed6124fe1fca05942f969f896027b63b87
gGiovanni Ortu <giovanni.ortu@abinsula.com>
2023-11-20 18:48:53 +0100

    Removed logs

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,10 @@ static uint8_t broadcastAddress[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 
 void receiveCallBackFunction(uint8_t *senderMac, uint8_t *incomingData, uint8_t len) 
 {
-  Serial.print("Received " );
   Serial.println( (char*) incomingData );
   uint8_t msg[] = "OK";
   uint8_t msg_len = sizeof(msg);
   esp_now_send(senderMac, msg, msg_len);
-
 }
 
 
@@ -25,21 +23,22 @@ void setup() {
   WiFi.mode(WIFI_STA);
 
   Serial.begin(115200);
-
+#ifdef VERBOSE
   Serial.println("ESP-Now Receiver");
   Serial.printf("Transmitter mac: %s\n", WiFi.macAddress().c_str());
-
+#endif
   if (esp_now_init() != 0) 
   {
+   
     Serial.println("ESP_Now init failed...");
     delay(RETRY_INTERVAL);
     ESP.restart();
   }
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
   esp_now_register_recv_cb(receiveCallBackFunction);
-
+#ifdef VERBOSE
   Serial.println("MASTER ready. Waiting for messages...");
-
+#endif
 
 }
 
